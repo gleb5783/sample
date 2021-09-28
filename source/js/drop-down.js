a8223ff09f9f631df981a19f2fddc028869fe143
gleb5783 <gleb5783@mail.ru>
2021-09-28 15:46:56 +0300

    test

--- source/js/drop-down.js
+++
@@ -1,8 +0,0 @@
-var allDropDownBtn = document.querySelectorAll('.about-us__item-button');
-
-allDropDownBtn.forEach((element) => element.addEventListener('click', () => {
-    var parent = element.closest('li').querySelector('p');
-    element.classList.toggle('about-us__item-button--open');
-    parent.classList.toggle('about-us__text--hidden');
-  })
-);
